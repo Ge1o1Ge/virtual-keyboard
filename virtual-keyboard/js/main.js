ef02e555952cb59cbd0bfcf4da7f8aa5346e019d
Georg II <118926055+Ge1o1Ge@users.noreply.github.com>
2023-05-01 14:25:39 +0200

    feat: impliment shift switch

--- virtual-keyboard/js/main.js
+++ virtual-keyboard/js/main.js
@@ -66,6 +66,15 @@ const keyboard = {
       ["Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Up", "Shift"],
       ["Ctrl", "Win", "Alt", "space", "Alt", "Left", "Down", "Right", "Ctrl"]
     ];
+
+    const shiftKeys = [
+      ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "none"],
+      ["none", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "{", "}", "|", "none"],
+      ["none", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", ":", `"`, "none"],
+      ["none", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "symbol", "<", ">", "?", "none", "none"],
+      ["none", "none", "none", "none", "none", "none", "none", "none", "none"]
+    ];
+
     const fragment = document.createDocumentFragment();
 
     for (let i = 0; i < keys.length; i++) {
@@ -75,6 +84,15 @@ const keyboard = {
         let key = document.createElement("button");
         key.classList.add("keyboard__key");
         key.classList.add(`keyboard__key-${keys[i][j].toLowerCase()}`);
+
+        key.setAttribute("data_key-basick", keys[i][j])
+        key.setAttribute("data_key-shifted", shiftKeys[i][j])
+
+        /**
+        key.setAttribute("data_key-basickru", keys[i][j])
+        key.setAttribute("data_key-shiftedru", keys[i][j])
+        */
+
         key.innerHTML = keys[i][j];
 
         switch (keys[i][j]) {
@@ -111,9 +129,74 @@ const keyboard = {
             })
             break;
 
+          case "Tab":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+              this.properties.value += "  ";
+              this._triggerEv("oninput");
+            })
+            break;
+
+          case "Shift":
+            key.setAttribute("special", true);
+            key.classList.add("keyboard__key--swith");
+            key.addEventListener("click", () => {
+              this._toggleShift();
+              key.classList.toggle("keyboard__key--active", this.properties.shift);
+            })
+            break;
+
+          case "Ctrl":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Win":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Alt":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Up":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Left":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Down":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Right":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
+          case "Del":
+            key.setAttribute("special", true);
+            key.addEventListener("click", () => {
+            })
+            break;
+
           default:
             key.addEventListener("click", () => {
-              this.properties.value += this.properties.capsLock ? keys[i][j].toUpperCase() : keys[i][j].toLowerCase();
+              this.properties.value += key.textContent;
               this._triggerEv("oninput");
             })
             break;
@@ -132,15 +215,38 @@ const keyboard = {
     }
   },
 
-  _toggleCaps() {
-    this.properties.capsLock = !this.properties.capsLock;
+  _toggleShift() {
+    this.properties.shift = !this.properties.shift;
 
     for (const key of this.elements.keys) {
-      if (!key.getAttribute("special")) {
+      if (key.getAttribute("data_key-shifted") == "symbol") {
+        if (this.properties.capsLock && this.properties.shift) {
+          key.textContent = key.textContent.toLowerCase()
+        } else if (!this.properties.capsLock && this.properties.shift) {
+          key.textContent = key.textContent.toUpperCase();
+        } else if (this.properties.capsLock && !this.properties.shift) {
+          key.textContent = key.textContent.toUpperCase();
+        } else if (!this.properties.capsLock && !this.properties.shift) {
+          key.textContent = key.textContent.toLowerCase()
+        }
+      } else if (key.getAttribute("data_key-shifted") !== "none") {
+        if (this.properties.shift) {
+          key.textContent = key.getAttribute("data_key-shifted")
+        } else {
+          key.textContent = key.getAttribute("data_key-basick")
+        }
+      }
+    }
+  },
+
+  _toggleCaps() {
+    this.properties.capsLock = !this.properties.capsLock;
+    for (const key of this.elements.keys) {
+      if (key.getAttribute("data_key-shifted") == "symbol") {
         key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
       }
     }
-  }
+  },
 }
 
 
